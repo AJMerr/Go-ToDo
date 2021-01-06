edc20eb1864cc19030bc09c83f075b689557dbef
Austin <austinm9506@gmail.com>
2021-01-06 04:40:32 -0500

    Created a local MongoDB database and added functionality for the POST and GET all API

--- server.go
+++ server.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var client *mongo.Client
 
 // Structs for a basic to do note
 
 type Note struct {
-	User        string `json:"user"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	User        string             `json:"user,omitempty" bson:"user,omitempty"`
+	Title       string             `json:"title,omitempty" bson:"title,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"`
 }
 
 // Creates a variable for notes to be a slice of the Note Struct
@@ -24,6 +33,9 @@ var notes = []Note{}
 func main() {
 
 	// Sets a varibale router, that creates a new router
+	clientOptions := options.Client().ApplyURI("mongodb://localhost/test")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, clientOptions)
 	router := mux.NewRouter()
 
 	router.HandleFunc("/notes", addNote).Methods("POST")
@@ -41,20 +53,49 @@ func main() {
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
-// Posts a note to
+// Posts a note
 func addNote(w http.ResponseWriter, r *http.Request) {
 	var newNote Note
+	_ = json.NewDecoder(r.Body).Decode(&newNote)
+	collection := client.Database("GoToDo").Collection("notes")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, newNote)
+
+	// Reads the JSON request body
 	json.NewDecoder(r.Body).Decode(&newNote)
 
+	// Appends the Notes Struct
 	notes = append(notes, newNote)
 
+	// Sets the HTTP header to JSON
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode(notes)
+	// Sends a JSON response
+	json.NewEncoder(w).Encode(result)
 }
 
 // Gets all data
 func getAllNotes(w http.ResponseWriter, r *http.Request) {
+	var notes []Note
+	collection := client.Database("GoToDo").Collection("notes")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var note Note
+		cursor.Decode(&note)
+		notes = append(notes, note)
+	}
+	if err := cursor.Err(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(notes)
 }
